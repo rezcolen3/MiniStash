9578201722947b1aab5e2b7679df9b51da701810
RezHackXYZ <rhythmupa@gmail.com>
2025-08-29 07:13:23 +0530

    new ui kinda!

--- src/routes/app/components/files.svelte
+++ src/routes/app/components/files.svelte
@@ -1,20 +1,21 @@
 <script>
-	let test = [
-		{ name: 'file 1', is_folder: false, id: 4324 },
-		{ name: 'file 90', is_folder: false, id: 4324 },
-		{ name: 'file 564', is_folder: false, id: 4324 },
-		{ name: 'folder 1', is_folder: true, id: 4324 },
-		{ name: 'folder 6857', is_folder: true, id: 4324 },
-	];
+	import { Files } from '../store.js';
 </script>
 
-{#each test as file}
+{#each $Files as file}
 	<div class="flex cursor-pointer items-center justify-between rounded p-2 hover:bg-gray-700">
-		<div class="flex items-center gap-2">
+		<a
+			class="flex items-center gap-2"
+			onclick={() => {
+				if (file.is_folder) {
+					openFolder([...$Dir, file.name]);
+				}
+			}}
+		>
 			<i class="nf text-sm {file.is_folder ? 'nf-fa-folder' : 'nf-fa-file'} text-gray-500"></i>
 			<span class="font-medium">
 				{file.name}
 			</span>
-		</div>
+		</a>
 	</div>
 {/each}
--- src/routes/app/store.js
+++ src/routes/app/store.js
@@ -1,4 +1,16 @@
 import { writable } from 'svelte/store';
 
-export let currentFiles = writable([]);
-export let Dir = writable([""]);
+export let Dir = writable(['']);
+export let Files = writable([]);
+
+/*
+let test = [
+		{ name: 'file 1', is_folder: false, id: 4324 },
+		{ name: 'file 90', is_folder: false, id: 4324 },
+		{ name: 'file 564', is_folder: false, id: 4324 },
+		{ name: 'folder 1', is_folder: true, id: 4324 },
+		{ name: 'folder 6857', is_folder: true, id: 4324 },
+	];
+
+['', 'files', 'projects']
+*/
