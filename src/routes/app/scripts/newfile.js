16455b71b5e53b8c8777c07ed6e4b652cdf3b9f9
RezHackXYZ <rhythmupa@gmail.com>
2025-08-30 12:11:24 +0530

    eeeee

--- src/routes/app/scripts/newfile.js
+++ src/routes/app/scripts/newfile.js
@@ -1,17 +1,36 @@
+import toast from 'svelte-5-french-toast';
+import { get } from 'svelte/store';
+import { Dir, popup } from '../store.js';
+import { openFolder } from './openfolder';
+
 export async function handleUpload(file, filelocation) {
-    console.log(file, filelocation)
+	popup.set(null);
 
+	let uploadPromise = new Promise(async (resolve, reject) => {
+		const formData = new FormData();
+		formData.append('file', file);
+		formData.append('filelocation', filelocation);
 
-	const formData = new FormData();
-	formData.append('file', file);
-    formData.append('filelocation', filelocation);
+		console.log(formData);
 
-    console.log(formData)
+		const res = await fetch('/api/new-file', {
+			method: 'POST',
+			body: formData,
+		});
 
-	const res = await fetch('/api/new-file', {
-		method: 'POST',
-		body: formData,
+		const result = await res.json();
+
+		if (res.ok) {
+			openFolder(get(Dir));
+			resolve(result);
+		} else {
+			reject(result);
+		}
 	});
 
-	console.log(await res.json());
+	toast.promise(uploadPromise, {
+		loading: 'Uploading...',
+		success: 'File uploaded successfully!',
+		error: 'Failed to upload file.',
+	});
 }
